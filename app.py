35603b17d3c7871d2b1446aea9ddeb60798df66b
Satyajit Ghana <satyajitghana7@gmail.com>
2023-05-29 04:03:07 +0000

    add docker compose

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+import redis
 
 app = Flask(__name__)
 
+# set up a connection to Redis
+r = redis.Redis(host='redis', port=6379, db=0)
+
 @app.route('/')
 def index():
-    return render_template('index.html', url="https://cataas.com/cat/gif")
-
+    # increment the visitor count
+    r.incr('visits')
+    visits = r.get('visits').decode('utf-8')
+    
+    return render_template('index.html', url="https://cataas.com/cat/gif", visits=visits)
